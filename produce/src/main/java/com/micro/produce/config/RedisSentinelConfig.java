4e57b02b67a766afc350f9927778212e9feb1b54
178625120 <178625120@qq.com>
2020-08-16 21:58:04 +0800

    # redis lettuce

---
+++ produce/src/main/java/com/micro/produce/config/RedisSentinelConfig.java
@@ -0,0 +1,40 @@
+package com.micro.produce.config;
+
+import io.lettuce.core.RedisURI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+
+import java.time.Duration;
+
+/**
+ * @author wangqianlong
+ * @create 2020-08-16 19:45
+ */
+@Configuration
+public class RedisSentinelConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettucePoolingClientConfiguration lettuceClientConfiguration =
+                LettucePoolingClientConfiguration.builder()
+                        .shutdownTimeout(Duration.ofMillis(100))
+                        .commandTimeout(Duration.ofSeconds(RedisURI.DEFAULT_TIMEOUT))
+                        .build();
+
+        //哨兵
+        RedisSentinelConfiguration sentinelConfiguration = new RedisSentinelConfiguration()
+                .master("master")
+                .sentinel("127.0.0.1", 26379)
+                .sentinel("127.0.0.1", 26380)
+                .sentinel("localhost", 26381);
+        return new LettuceConnectionFactory(sentinelConfiguration,lettuceClientConfiguration);
+
+    }
+
+}
