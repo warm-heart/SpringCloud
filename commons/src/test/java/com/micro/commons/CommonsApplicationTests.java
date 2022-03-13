2d10a3ae026f89746c71960b15cffcdaab8c68fd
wufeng.wql <long.1122>
2022-03-13 20:48:21 +0800

    fix :发红包

--- commons/src/test/java/com/micro/commons/CommonsApplicationTests.java
+++ commons/src/test/java/com/micro/commons/CommonsApplicationTests.java
@@ -3,12 +3,33 @@ package com.micro.commons;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 @SpringBootTest
 class CommonsApplicationTests {
 
     @Test
     void contextLoads() {
+        List<BigDecimal> res = solution(99, 10);
+        BigDecimal total = res.stream().reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+        System.err.println(res);
+        System.err.println(total);
     }
 
-
+    public List<BigDecimal> solution(int money, int count) {
+        BigDecimal totalMoney = new BigDecimal(money);
+        BigDecimal loadFactory = new BigDecimal(10000);
+        BigDecimal once = totalMoney.divide(loadFactory, 2, BigDecimal.ROUND_HALF_DOWN);
+        int forCount = totalMoney.divide(once, 2, BigDecimal.ROUND_HALF_DOWN).intValue();
+        BigDecimal[] result = new BigDecimal[count];
+        for (int i = 1; i <= forCount; i++) {
+            int n = new Random().nextInt(count);
+            result[n] = result[n] == null ? once : result[n].add(once);
+        }
+        return Arrays.stream(result).collect(Collectors.toList());
+    }
 }
