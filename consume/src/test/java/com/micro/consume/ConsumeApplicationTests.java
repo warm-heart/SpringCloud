21e41a0b545164c3896bc152350bc7e2aedf2dc6
wufeng.wql <long.1122>
2022-03-14 16:47:47 +0800

    fix :数据库配置

--- consume/src/test/java/com/micro/consume/ConsumeApplicationTests.java
+++ consume/src/test/java/com/micro/consume/ConsumeApplicationTests.java
@@ -1,45 +1,25 @@
 package com.micro.consume;
 
+import com.micro.commons.dto.ApiResponse;
+import com.micro.commons.entity.User;
+import com.micro.consume.feign.produce.ProduceFeignService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import javax.annotation.Resource;
 
 @SpringBootTest
 class ConsumeApplicationTests {
 
+
+    @Resource
+    private ProduceFeignService produceFeignService;
+
     @Test
     void contextLoads() {
-        List list = solution(100, 10);
+        ApiResponse<User> res = produceFeignService.get("1");
+        System.err.println(res);
     }
 
-
-    public List<BigDecimal> solution(int maneySum, int redNum) {
-        ArrayList array = new ArrayList(redNum);
-        BigDecimal totalManey = new BigDecimal(maneySum);
-        BigDecimal halfMoney = totalManey.divide(new BigDecimal(2), 0, BigDecimal.ROUND_DOWN);
-        BigDecimal maxRedPage = new BigDecimal(90);
-        for (int i = 0; i < redNum; i++) {
-            Random random = new Random();
-            Double v = random.nextDouble();
-            BigDecimal bigDecimal = totalManey.multiply(new BigDecimal(v));
-            BigDecimal multiply = totalManey.multiply(bigDecimal);
-            if (maxRedPage.compareTo(multiply) >= 0) {
-                if (multiply.compareTo(halfMoney) > 0) {
-                    multiply = multiply.subtract(halfMoney);
-                }
-                if (multiply.compareTo(BigDecimal.ZERO) == 0) {
-                    continue;
-                }
-                multiply = multiply.setScale(2, BigDecimal.ROUND_DOWN);
-                array.add(multiply);
-                totalManey = totalManey.subtract(multiply);
-            }
-        }
-        return array;
-    }
 
 }
