023a8db0e961bcecd3f44c77854b056afe837d93
wufeng.wql <long.1122>
2022-06-22 14:27:19 +0800

    fix ：agent

---
+++ apm/src/main/java/com/microservice/apm/ApmApplication.java
@@ -0,0 +1,14 @@
+package com.microservice.apm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmApplication.class, args);
+	}
+
+}
+
